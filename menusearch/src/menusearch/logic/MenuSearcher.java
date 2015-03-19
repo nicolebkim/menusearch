6f154d8caf996cf8fa595ce78df27e5d70245243
estevampg@hotmail.com <estevampg@hotmail.com@9b3f73b5-0408-34b4-e934-b4f5c39db870>
2015-03-19 03:43:21 +0000

    Logic method to search menus that receive parameters from GUI send it to a class to retrieve data from the database and then send the data to the GUI created.

---
+++ menusearch/src/menusearch/logic/MenuSearcher.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menusearch.logic;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author estevamgarcia
+ */
+public class MenuSearcher{
+    
+    public ArrayList<Menu> menus = new ArrayList();
+    
+    public ArrayList<Menu> MenuSearcher(String name, String sponsor, String event, String venue, String place, 
+            String physicalDescription, String occasion, String notes, String callNumber, String keywords,
+            String language, String menuDate, String location, String locationType, String currency,
+            String currencySymbol, String status, int pageCount, int dishCount){
+        
+        
+        ParametersMenu p = new ParametersMenu();
+        
+        p.nameToSearch(name);
+        p.sponsorToSearch(sponsor);
+        p.eventToSearch(event);
+        p.venueToSearch(venue);
+        p.physicalDescriptionToSearch(physicalDescription);
+        p.occasionToSearch(occasion);
+        p.notesToSearch(notes);
+        p.callNumberToSearch(callNumber);
+        p.keywordsToSearch(keywords);
+        p.languageToSearch(language);
+        p.menuDateToSearch(menuDate);
+        p.locationToSearch(location);
+        p.locationTypeToSearch(locationType);
+        p.currencyToSearch(currency);
+        p.currencySymbolToSearch(currencySymbol);
+        p.statusToSearch(status);
+        p.pageCountToSearch(pageCount);
+        p.dishCountToSearch(dishCount);
+        
+        menus = searchMenuDB(p);
+        
+        return menus;
+        
+        
+    }
+}
